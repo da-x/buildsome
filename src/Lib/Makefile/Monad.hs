34ea4f8a5baa41327ec068c0600353fa1386ade9
Dan Aloni <alonid@gmail.com>
2016-02-23 20:09:46 +0200

    Remove %3 out of makefile including reading time

--- src/Lib/Makefile/Monad.hs
+++ src/Lib/Makefile/Monad.hs
@@ -13,6 +13,7 @@ import Control.Monad.Trans.State (StateT(..))
 import Data.Binary (Binary)
 import Data.ByteString (ByteString)
 import Data.Map (Map)
+import Data.IORef (newIORef, IORef, readIORef, modifyIORef')
 
 import GHC.Generics (Generic)
 import Lib.Directory (getMFileStatus)
@@ -24,6 +25,7 @@ import qualified Data.ByteString.Char8 as BS8
 import qualified Data.Map as Map
 import qualified System.IO as IO
 import qualified System.Posix.ByteString as Posix
+import Control.Exception ( SomeException )
 
 -- Specific Monad type for makefile parsing that tracks all inputs/outputs:
 data PutStrLnTarget = Out | Err deriving (Generic, Show)
@@ -40,11 +42,16 @@ type ReadFiles = Map FilePath (Maybe Posix.FileStatus)
 data W = W
   { wPutStrLns :: [PutStrLn]
   , wReadFiles :: Map FilePath [Maybe Posix.FileStatus]
+  , wFiles    :: Maybe (IORef (Map FilePath (Either SomeException ByteString, Maybe Posix.FileStatus)))
   }
+
 instance Monoid W where
-  mempty = W mempty mempty
-  mappend (W ax ay) (W bx by) =
-    W (mappend ax bx) (Map.unionWith (++) ay by)
+  mempty = W mempty mempty Nothing
+  mappend (W ax ay Nothing) (W bx by bz) =
+    W (mappend ax bx) (Map.unionWith (++) ay by) bz
+  mappend (W ax ay az) (W bx by Nothing) =
+    W (mappend ax bx) (Map.unionWith (++) ay by) az
+  mappend _ _ = error "internal error"
 
 runPutStrLn :: PutStrLn -> IO ()
 runPutStrLn (PutStrLn target bs) = BS8.hPutStrLn (targetHandle target) bs
@@ -68,9 +75,27 @@ instance MonadMakefileParser M where
   outPutStrLn = doPutStrLn Out
   errPutStrLn = doPutStrLn Err
   tryReadFile filePath = do
-    mFileStat <- M $ liftIO $ getMFileStatus filePath
-    tell $ mempty { wReadFiles = Map.singleton filePath [mFileStat] }
-    M $ liftIO $ tryReadFile filePath
+    w <- M State.get
+    let miss = do
+          mFileStat <- M $ liftIO $ getMFileStatus filePath
+          tell $ mempty { wReadFiles = Map.singleton filePath [mFileStat] }
+          mFileData <- M $ liftIO $ tryReadFile filePath
+          let v = (mFileData, mFileStat)
+          case wFiles w of
+              Just ioref -> M $ liftIO $ modifyIORef' ioref (Map.insert filePath v)
+              Nothing -> return ()
+          return v
+        hit (content, mFileStat) = do
+          tell $ mempty { wReadFiles = Map.singleton filePath [mFileStat] }
+          return content
+
+    case wFiles w of
+        Just ioref ->
+            do m <- M $ liftIO $ readIORef ioref
+               case Map.lookup filePath m of
+                   Nothing -> fmap fst miss
+                   Just content -> hit content
+        Nothing -> fmap fst miss
 
 mergeAllFileStatuses :: FilePath -> [Maybe Posix.FileStatus] -> IO (Maybe Posix.FileStatus)
 mergeAllFileStatuses filePath = go
@@ -84,6 +109,7 @@ mergeAllFileStatuses filePath = go
 
 runM :: M a -> IO (ReadFiles, [PutStrLn], a)
 runM (M act) = do
-  (res, w) <- runStateT act mempty
+  ref <- newIORef Map.empty
+  (res, w) <- runStateT act mempty { wFiles = Just ref }
   readFiles <- Map.traverseWithKey mergeAllFileStatuses (wReadFiles w)
   return (readFiles, wPutStrLns w, res)
